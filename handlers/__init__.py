3ceda70c132f703cbf12a20e5f8923945344a8d4
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-11-15 17:46:59 +0000

    Added collection handler

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,4 @@
 from .home import home_handler
 from .search import search_handler
-from .structures import structure_view_handler, structure_lookup_handler, structure_browse_handler
+from .structures import structure_view_handler, structure_lookup_handler, structure_browse_handler, structure_collection_handler
+from .errors import handle_404
